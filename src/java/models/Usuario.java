c2b5fcae335b46d5d9446e79dbfdde0a2ecdc842
Bernardo Ladeira Kartabil <beladeira1.000@gmail.com>
2025-08-21 18:43:58 -0300

    Usuario.java v0.1

--- src/java/models/Usuario.java
+++ src/java/models/Usuario.java
@@ -11,6 +11,64 @@ public class Usuario implements Registro
     private String HashSenha;
     private String PerguntaSecreta;
     private String RespostaSecreta;
+    
 
+     // ---------- Construtores ----------
+    public Usuario() {
+        this.id = -1;
+        this.nome = "";
+        this.email = "";
+        this.HashSenha = "";
+        this.PerguntaSecreta = "";
+        this.RespostaSecreta = "";
+
+    }
+
+     public Usuario(String nome, String email, String senha, String pergunta, String resposta) {
+        this.id = -1; // será definido pelo Arquivo<T>
+        this.nome = nome;
+        this.email = email;
+        this.HashSenha = senha;
+        this.PerguntaSecreta = pergunta;
+        this.RespostaSecreta = resposta;
+
+    }
+
+     // ---------- Implementação da interface Registro ----------
+    @Override
+    public int getID() {
+        return id;
+    }
+
+    @Override
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    /*@Override
+    public byte[] toByteArray() throws Exception {} // falta implementar
+
+    @Override
+    public void fromByteArray(byte[] ba) throws Exception {} // falta implementar 
+
+    */
+
+
+
+    // ---------- Getters e setters ----------
+
+    public int getId() { return id; }
+
+    public String getNome() { return nome; }
+
+    public String getEmail() { return email; }
+
+    public String getHashSenha() { return HashSenha; }
+
+    public String getPerguntaSecreta() { return PerguntaSecreta; }
+
+    public String getRespostaSecreta() { return RespostaSecreta; }
+
+    
 
 }
