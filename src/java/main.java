9ab940946bacc5087216a7cbd2682d2ebdbcd848
Bernardo Ladeira Kartabil <beladeira1.000@gmail.com>
2025-08-21 18:50:54 -0300

    Adicionando Terminal

--- src/java/main.java
+++ src/java/main.java
@@ -1,5 +1,16 @@
 package src.java;
+import src.java.ui.*;
+
+import java.util.Scanner;
+
+import src.java.ui.Terminal;
 
 public class main {
-    
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        Terminal terminal = new Terminal();
+
+        terminal.exibirTelaInicial(scanner);
+    }
 }
