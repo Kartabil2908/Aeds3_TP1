03ac7051a44ce565698be2664f7792fcce3deca8
Yasmin Moreira <yasmin.santos.1484596@sga.pucminas.br>
2025-10-20 22:11:26 -0300

    Atualização Main.java

--- Aeds3_TP1/Main.java
+++ Aeds3_TP1/Main.java
@@ -1,3 +1,6 @@
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 import src.presenteFacil.controller.ControladorUsuario;
 import src.presenteFacil.view.MenuInicial;
@@ -26,7 +29,25 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+        // Ajusta Entrada/Saída para a codificação real do console (Windows/Unix)
+        Charset consoleCs = null;
+        
+        try {
+            if (System.console() != null) {
+                consoleCs = System.console().charset();
+            }
+        } catch (Throwable ignored) { }
+
+        if (consoleCs != null) {
+            try {
+                System.setOut(new PrintStream(System.out, true, consoleCs));
+                System.setErr(new PrintStream(System.err, true, consoleCs));
+            } catch (Exception ignored) { }
+        }
+
+        Scanner scanner = (consoleCs != null)
+                ? new Scanner(new InputStreamReader(System.in, consoleCs))
+                : new Scanner(System.in);
 
         try {
             ControladorUsuario userController = new ControladorUsuario();
